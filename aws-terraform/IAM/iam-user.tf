7d24f96b0d5940145857aa2688bda925f2ca0e0f
elyas-syed <isyedwork@outlook.com>
2025-01-16 08:47:07 +0000

    updated ec2 task

--- aws-terraform/IAM/iam-user.tf
+++ aws-terraform/IAM/iam-user.tf
@@ -26,3 +26,4 @@ resource "aws_iam_user_policy_attachment" "test-attach" {
   user       = aws_iam_user.user.name
   policy_arn = aws_iam_policy.policy.arn
 }
+
--- terraform-ec2-tasks/ec2-task1/ec2-task1.tf
+++ terraform-ec2-tasks/ec2-task1/ec2-task1.tf
@@ -29,8 +29,6 @@ resource "aws_route53_record" "wordpress" {
   ]
 }
 
-
-
 resource "aws_key_pair" "wordpress" {
   key_name   = "wordpress"
   public_key = file("~/.ssh/id_rsa.pub")
