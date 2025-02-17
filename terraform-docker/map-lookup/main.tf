a4b3388e632e640f53cdd9a4e3445395d88e9679
elyas-syed <isyedwork@outlook.com>
2025-02-17 10:04:29 +0000

    added map-lookup tf

---
+++ terraform-docker/map-lookup/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+
+provider "docker" {}
+
+resource "null_resource" "dockervol" {
+  provisioner "local-exec" {
+    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+  }
+}
+
+resource "docker_image" "nodered_image" {
+  name = lookup(var.image, var.env)
+}
+
+resource "random_string" "random" {
+  count   = local.container_count
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
+resource "docker_container" "nodered_container" {
+  count = local.container_count
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = var.int_port
+    external = var.ext_port[count.index]
+  }
+  volumes {
+    container_path = "/data"
+    host_path      = "${path.cwd}/noderedvol"
+  }
+}
+
+
+# Outputs
+output "container-name" {
+  value       = docker_container.nodered_container[*].name
+  description = "The name of the container"
+}
+
+output "ip-address" {
+  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+  description = "The IP address and external port of the container"
+}
