cb4c150acb416865b2ad4377fde562d97db57912
elyas-syed <isyedwork@outlook.com>
2025-03-20 14:42:24 +0000

    added docker directory

---
+++ terraform-docker/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
