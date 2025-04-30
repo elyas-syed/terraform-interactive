b53e17eab05d2df65fe577b204d8eb8ddbdd523b
elyas-syed <isyedwork@outlook.com>
2025-04-30 21:33:17 +0000

    completed ec2-datasource

--- aws-terraform/ec2-datasource/outputs.tf
+++ aws-terraform/ec2-datasource/outputs.tf
@@ -17,7 +17,7 @@ output "ec2_instance_output" {
         Machine has the following IP address   ${aws_instance.web.public_ip}
         Machine has the following ID           ${aws_instance.web.id}
         Machine is in                          ${aws_instance.web.availability_zone}
-        Machine uses following key pair        ${aws_key_pair.web.key_name}
+        Machine uses following key pair        ${data.aws_key_pair.example.key_name}
         Machine dns name is                    ${aws_instance.web.public_dns}
         EOF
 }
