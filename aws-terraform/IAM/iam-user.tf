8d8764d59226cbb9f7115a2d04aa872876cc84c3
elyas-syed <isyedwork@outlook.com>
2025-05-02 09:22:52 +0000

    added required providers code

--- aws-terraform/IAM/iam-user.tf
+++ aws-terraform/IAM/iam-user.tf
@@ -5,7 +5,7 @@ resource "aws_iam_user" "user" {
 resource "aws_iam_policy" "policy" {
   name        = "test-policy"
   description = "A test policy"
-  policy = <<EOF
+  policy      = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
