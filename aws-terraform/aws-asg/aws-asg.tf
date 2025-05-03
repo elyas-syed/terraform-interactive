710fea5235d99f95e54fe14e3b312a168456c88e
elyas-syed <isyedwork@outlook.com>
2025-05-03 22:21:24 +0000

    added aws-asg

---
+++ aws-terraform/aws-asg/aws-asg.tf
@@ -0,0 +1,18 @@
+resource "aws_autoscaling_group" "example" {
+  name                 = var.cluster_name
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
+
+  min_size = var.min_size
+  max_size = var.max_size
+
+  # Use instance refresh to roll out changes to the ASG
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+  }
+}
